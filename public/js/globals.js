7a2b4ccc9e6ebe8dfc51bed72f8b5658ff036776
Brian Diaz <bidiaz101@gmail.com>
2022-02-05 08:51:01 -0600

    created globals

---
+++ public/js/globals.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d') // used for drawing
+const animate = window.requestAnimationFrame // use for animating
+
+// game window details
+
+const GAME_HEIGHT = 600
+const GAME_WIDTH = 800
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,12 @@
+function drawBackground(){
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT)
+}
+
+function setup(){
+    drawBackground()
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    alert("index.js wired")
+    setup()
 })
