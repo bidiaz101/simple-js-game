0e3728ee3d079cb52e161c98be29a9dd9c16a9db
Brian Diaz <bidiaz101@gmail.com>
2022-02-05 04:26:09 -0600

    wired up express server to js and html file

---
+++ index.js
@@ -0,0 +1,15 @@
+// Creates a controller
+const express = require('express')
+const app = express()
+
+// Tells controller to know about files in public directory
+app.use(express.static(__dirname + '/public'))
+
+// Tells controller to send index.html as response when a get request is made to the root
+app.get('/', function(req, resp){
+    resp.sendFile('index.html')
+})
+
+app.listen(3000, function(){
+    console.log('listening on port: 3000')
+})
